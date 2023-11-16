c2af71c63055d9859d6b984b87115b3dde9df60c
Topher-Two-Four <tophertwofour@gmail.com>
2023-11-16 11:08:53 -0700

    Added buttons, sprites, and text to potion crafting test scene and script

--- Assets/Scripts/PotionCraftingSystem.cs
+++ Assets/Scripts/PotionCraftingSystem.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PotionCraftingSystem : MonoBehaviour
 {
@@ -23,17 +24,47 @@ public class PotionCraftingSystem : MonoBehaviour
     public float mediumQualityTimeLimit = 50f;
     public float lowQualityTimeLimit = 30f;
 
+    public bool isBrewing = false;
+
+    public Button ingredient1Button;
+    public Button ingredient2Button;
+    public Button ingredient3Button;
+    public Button potionRetrievalButton;
+    public Button increaseTempButton;
+    public Button decreaseTempButton;
     public Button brewButton;
+
+    public Sprite ingredient1Image;
+    public Sprite ingredient2Image;
+    public Sprite ingredient3Image;
+    public Sprite potionImage;
+
+    public TMP_Text temperatureDisplayText;
+    public TMP_Text timeRemainingText;
 
 
     private void Awake()
     {
-            brewButton.onClick.AddListener(() => BrewPotion(ingredient1, ingredient2, ingredient3));
+            ingredient1Button.onClick.AddListener(() => AddIngredientToInventory());
+            ingredient2Button.onClick.AddListener(() => AddIngredientToInventory());
+            ingredient3Button.onClick.AddListener(() => AddIngredientToInventory());
+            //increaseTempButton.onClick.AddListener(() => AddIngredientToInventory());
+            //decreaseTempButton.onClick.AddListener(() => AddIngredientToInventory());
+            brewButton.onClick.AddListener(() => BrewPotion(ingredient1, ingredient2, ingredient3)); // Add button listener to brew potion when pressed
     }
 
     private void Update()
     {
-        SetBrewButtonStatus();
+        UpdateBrewButtonStatus(); // NEED TO MAKE DO THIS ONLY WHEN CHANGE OCCURS
+    }
+
+    public void AddIngredientToSlot(ItemData ingredient)
+    {
+
+    }
+    public void AddIngredientToInventory()
+    {
+
     }
 
     public int CheckPotionQuality(int cookTime, float timeAtDesiredTemp)
@@ -68,24 +99,53 @@ public class PotionCraftingSystem : MonoBehaviour
 
     public void BrewPotion(ItemData ingredient1, ItemData ingredient2, ItemData ingredient3)
     {
-
-
+        isBrewing = true;
+        timeCooked = 0;
+        timeAtDesiredTemp = 0;
+        Recipe potionRecipe = RecipeList.Instance.FindRecipe(ingredient1, ingredient2, ingredient3);
+        desiredTemp = potionRecipe.desiredTemp;
+        cookTime = potionRecipe.cookTime;
+        if (timeCooked > cookTime)
+        {
+            // Display image of potion that is being made
+            timeRemainingText.GetComponent<TMPro.TextMeshProUGUI>().text = ((cookTime - timeCooked).ToString());
+            if (desiredTemp == currentTemp)
+            {
+                timeAtDesiredTemp++;
+            }
+            brewButton.interactable = false;
+        } 
+        else
+        {
+            brewButton.interactable = true;
+            timeRemainingText.GetComponent<TMPro.TextMeshProUGUI>().text = ("Done!");
+            // Make potion retrievable
+        }
     }
 
-    public void SetBrewButtonStatus()
+    public void UpdateBrewButtonStatus()
     {
         Recipe potionRecipe = RecipeList.Instance.FindRecipe(ingredient1, ingredient2, ingredient3);
         if (potionRecipe != null)
         {
             brewButton.interactable = true;
-            //ItemData potionBrewed = new ItemData();
-            RecipeList.Instance.AddPotionToInventory(potionRecipe);
-
         }
         else
         {
             // Do not allow making of potion and keep brew button grayed out
             brewButton.interactable = false;
+        }
+    }
+    public void ResetPotionRetrievalArea()
+    {
+        Recipe potionRecipe = RecipeList.Instance.FindRecipe(ingredient1, ingredient2, ingredient3);
+        if (isBrewing)
+        {
+            potionImage = potionRecipe.potionIcon;
+        }
+        else
+        {
+            potionImage = null;
         }
     }
 
--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -10,6 +10,9 @@ public class Recipe : ScriptableObject
     public ItemData ingredient2 = null;
     public ItemData ingredient3 = null;
 
+    public float cookTime = 120f;
+    public int desiredTemp = 3;
+
     public Sprite potionIcon;
 
     // INSERT REMAINDER OF ITEM OBJECT DATA HERE

4261c4e88750fa1763c9d277823dac5c81372d3f
Topher-Two-Four <tophertwofour@gmail.com>
2023-11-28 10:57:54 -0700

    Created potion type enum class, added health potion quest scriptable object and quest step

---
+++ Assets/Scripts/QuestSystem/Events/PotionEvents.cs
@@ -0,0 +1,24 @@
+using System;
+
+public class PotionEvents
+{
+    /*
+    public event Action<int> onGoldGained;
+    public void GoldGained(int gold) 
+    {
+        if (onGoldGained != null) 
+        {
+            onGoldGained(gold);
+        }
+    }
+
+    public event Action<int> onGoldChange;
+    public void GoldChange(int gold) 
+    {
+        if (onGoldChange != null) 
+        {
+            onGoldChange(gold);
+        }
+    }
+    */
+}
---
+++ Assets/Scripts/QuestSystem/QuestSystem/PotionType.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PotionType
+{
+    HEALTH,
+    LOVE,
+    HATRED,
+    ANTIDOTE,
+    POISON,
+    DEATH,
+    LUCKY,
+    BENEFIT,
+    CRIPPLING,
+}

d4963859d023ed23163ef81ccb9ad10076061a2e
Topher-Two-Four <tophertwofour@gmail.com>
2023-12-02 03:57:20 -0700

    Added all potion recipes to project

--- Assets/Scripts/PotionSystem/Recipe.cs
+++ Assets/Scripts/PotionSystem/Recipe.cs
@@ -11,6 +11,7 @@ public class Recipe : ScriptableObject
     public ItemData ingredient1; // First ingredient in this recipe
     public ItemData ingredient2; // Second ingredient in this recipe
     public ItemData ingredient3; // Third ingredient in this recipe
+    public ItemData ingredient4; // Fourth ingredient in this recipe
 
     public float cookTime = 120f; // How long the recipe takes to cook
     public int desiredTemp = 3; // The desired temperature for this potion between 1 and 5

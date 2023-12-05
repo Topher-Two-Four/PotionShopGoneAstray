43b2b3159f0f4482393a75c6fb86ac2f72a4705b
Topher-Two-Four <tophertwofour@gmail.com>
2023-12-04 20:38:17 -0700

    Created recipe book manager for holding recipe book pages and turning them, began implementing page UI in potion shop scene

---
+++ Assets/Scripts/RecipeBookManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeBookManager : MonoBehaviour
+{
+    public Image emptyRecipeBookPage;
+    public Image frontTitleRecipeBookPage;
+    public Image endRecipeBookPage;
+    public Image[] ingredientPages;
+    public Image[] potionPages;
+
+
+    void Start()
+    {
+        
+    }
+
+}

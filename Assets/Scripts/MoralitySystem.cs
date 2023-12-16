2874648543762a62a500d9c921cbf09eae74fb6a
Topher-Two-Four <tophertwofour@gmail.com>
2023-12-16 13:38:37 -0700

    UI displays reputation in top right corner of screen, changes color and text

--- Assets/Scripts/MoralitySystem.cs
+++ Assets/Scripts/MoralitySystem.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoralitySystem : MonoBehaviour
 {
-
     public int moralityCounter = 0;
 
+    public TMP_Text moralityDisplayText;
+
     public static MoralitySystem Instance { get; private set; } // Singleton logic
+
+    private void Start()
+    {
+        UpdateMoralityUI();
+    }
 
     private void Awake()
     {
@@ -50,6 +57,47 @@ public class MoralitySystem : MonoBehaviour
         else if (potionData.isDeath)
         {
             moralityCounter -= 4;
+        }
+
+        UpdateMoralityUI();
+    }
+
+    private void UpdateMoralityUI()
+    {
+        if (moralityCounter >= 10)
+        {
+            moralityDisplayText.text = "Alignment: All Good (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(0, 255, 0);
+        }
+        else if (moralityCounter >= 5 && moralityCounter < 10)
+        {
+            moralityDisplayText.text = "Alignment: Very Good (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(255, 130, 0);
+        }
+        else if (moralityCounter >= 1 && moralityCounter < 5)
+        {
+            moralityDisplayText.text = "Alignment: Good (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(255, 255, 0);
+        }
+        else if (moralityCounter >= -1 && moralityCounter < 1)
+        {
+            moralityDisplayText.text = "Alignment: Neutral (" + moralityCounter + ")";
+            moralityDisplayText.color = Color.gray;
+        }
+        else if (moralityCounter >= -5 && moralityCounter < -1)
+        {
+            moralityDisplayText.text = "Alignment: Bad (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(255, 203, 0);
+        }
+        else if (moralityCounter > -10 && moralityCounter < -5)
+        {
+            moralityDisplayText.text = "Alignment: Very Bad (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(255, 110, 0);
+        }
+        else
+        {
+            moralityDisplayText.text = "Alignment: All Bad (" + moralityCounter + ")";
+            moralityDisplayText.color = new Color(255, 0, 0);
         }
     }
 

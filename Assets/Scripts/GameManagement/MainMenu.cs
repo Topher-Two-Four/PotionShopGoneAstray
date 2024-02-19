b46dd9ad328e8e8651f19d290ef375d7d612cbf5
Topher-Two-Four <tophertwofour@gmail.com>
2024-02-19 09:12:21 -0700

    Credits page and settings page added to main menu as UI canvases that are able to be toggled using the main menu script

--- Assets/Scripts/GameManagement/MainMenu.cs
+++ Assets/Scripts/GameManagement/MainMenu.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Main Menu Settings:")]
+    [Tooltip("The canvas that holds the main menu UI elements.")]
+    [SerializeField] private GameObject mainMenuCanvas;
+    [Tooltip("The canvas that holds the settings UI elements.")]
+    [SerializeField] private GameObject settingsCanvas;
+    [Tooltip("The canvas that holds the credits UI elements.")]
+    [SerializeField] private GameObject creditsCanvas;
+
     public void Start()
     {
         AudioManager.Instance.PlayMusic("MenuMusic");
@@ -22,10 +30,25 @@ public class MainMenu : MonoBehaviour
 
     public void OnSettingsPressed()
     {
-        GameManager.Instance.SwitchSceneToSettingsMenu();
+        mainMenuCanvas.gameObject.SetActive(false);
+        settingsCanvas.gameObject.SetActive(true);
+        creditsCanvas.gameObject.SetActive(false);
+    }
+    public void OnCreditsPressed()
+    {
+        mainMenuCanvas.gameObject.SetActive(false);
+        settingsCanvas.gameObject.SetActive(false);
+        creditsCanvas.gameObject.SetActive(true);
     }
 
-    public void OnQuitPressed()
+    public void OnReturnToMainMenuPressed()
+    {
+        mainMenuCanvas.gameObject.SetActive(true);
+        settingsCanvas.gameObject.SetActive(false);
+        creditsCanvas.gameObject.SetActive(false);
+    }
+
+        public void OnQuitPressed()
     {
         Application.Quit();
     }

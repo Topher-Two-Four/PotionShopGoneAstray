980670b85b801d4dcc185e6cb94033664da69f1c
Topher-Two-Four <tophertwofour@gmail.com>
2024-03-09 07:55:30 -0700

    Designed and implemented way for player to dismiss tutorial windows by clicking on them

---
+++ Assets/Scripts/UI/CloseTutorialOnClick.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseTutorialOnClick : MonoBehaviour
+{
+    private Button _tutorialWindowButton;
+    private GameObject _tutorialWindow;
+
+    void Start()
+    {
+        if (gameObject.GetComponent<Button>() != null)
+        {
+            _tutorialWindow = gameObject;
+            _tutorialWindowButton = gameObject.GetComponent<Button>();
+            _tutorialWindowButton.onClick.AddListener(() => CloseTutorialWindow());
+        }
+    }
+
+    private void CloseTutorialWindow()
+    {
+        _tutorialWindow.SetActive(false);
+    }
+
+}

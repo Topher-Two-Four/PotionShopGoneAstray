f435b99bd90912a87d8600bbee48151af06fc4ec
Topher-Two-Four <tophertwofour@gmail.com>
2024-03-23 12:31:24 -0600

    Implemented condition in potion shop load on trigger to respawn the player into another maze if they fall through the world

--- Assets/Scripts/LevelManagement/LoadPotionShopOnTrigger.cs
+++ Assets/Scripts/LevelManagement/LoadPotionShopOnTrigger.cs
@@ -8,6 +8,7 @@ public class LoadPotionShopOnTrigger : MonoBehaviour
     [Header("Time Removed On Trigger:")]
     [Tooltip("The amount of time to remove when collision with player occurs.")]
     [SerializeField] private float caughtTimeRemoveAmount = 120.0f;
+    [SerializeField] private bool backToMaze = false;
 
     // Switch to potion shop scene on collision trigger with this game object
     private void OnTriggerEnter(Collider other)
@@ -17,9 +18,14 @@ public class LoadPotionShopOnTrigger : MonoBehaviour
             if (gameObject.tag == "FallBoundary")
             {
                 CutsceneManager.Instance.PlayFallCutscene();
+                GameManager.Instance.timeRemaining -= caughtTimeRemoveAmount; // Remove time from day if player is caught by enemy in maze
+                GameManager.Instance.SwitchSceneToPotionLevel(); // Switch scene to potion shop
             }
-            GameManager.Instance.timeRemaining -= caughtTimeRemoveAmount; // Remove time from day if player is caught by enemy in maze
-            GameManager.Instance.SwitchSceneToPotionLevel(); // Switch scene to potion shop
+            
+            if (backToMaze)
+            {
+                GameManager.Instance.SwitchSceneToMazeLevel();
+            }
         }
     }
 }
